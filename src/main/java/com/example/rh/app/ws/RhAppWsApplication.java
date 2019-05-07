421254e3325b22fbef7300d8ae08c32e0964879f
Ryller Oliveira <rylleroliveira@gmail.com>
2019-05-07 14:18:04 -0300

    Initial commit

---
+++ src/main/java/com/example/rh/app/ws/RhAppWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rh.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhAppWsApplication.class, args);
+	}
+
+}
