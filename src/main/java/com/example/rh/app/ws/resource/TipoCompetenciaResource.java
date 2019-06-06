f24dcf26b960cf93aa8a281313bd6cc7a04ea149
ryllerO <rylleroliveira@gmail.com>
2019-06-06 00:46:54 -0300

    implementando resources tipo competencia

--- src/main/java/com/example/rh/app/ws/resource/TipoCompetenciaResource.java
+++ src/main/java/com/example/rh/app/ws/resource/TipoCompetenciaResource.java
@@ -1,8 +1,50 @@
 package com.example.rh.app.ws.resource;
 
+import com.example.rh.app.ws.event.RecursoCriadoEvent;
+import com.example.rh.app.ws.model.TipoCompetencia;
+import com.example.rh.app.ws.repository.TipoCompetenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+
 /**
  * @author Ryller Oliveira
  */
-
+@RestController
+@RequestMapping("/tipo-competencia")
 public class TipoCompetenciaResource {
+
+    @Autowired
+    private TipoCompetenciaRepository tipoCompetenciaRepository;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @GetMapping
+    public List<TipoCompetencia> listar (){
+        return tipoCompetenciaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<TipoCompetencia> criar (
+            @Valid @RequestBody TipoCompetencia tipo, HttpServletResponse response ) {
+        TipoCompetencia tipoSalvo = tipoCompetenciaRepository.save(tipo);
+
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, tipoSalvo.getId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(tipoSalvo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TipoCompetencia> buscarPorId(@PathVariable Long id) {
+        TipoCompetencia tipo = tipoCompetenciaRepository.findOne(id);
+        return tipo != null ? ResponseEntity.ok(tipo) : ResponseEntity.notFound().build();
+    }
+
 }
